98a144d669268b5f7011babd4c36091d492808c9
Eugenio Barquin <eu.barquin@me.com>
2016-12-24 11:55:09 +0100

    Creamos la lógica. Al pulsar el botón de cada acorde se muestra la representación gráfica.

--- Chords/GuitarViewController.swift
+++ Chords/GuitarViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 class GuitarViewController: UIViewController {
 
     @IBOutlet weak var imgGuitarChord: UIImageView!
-    
     @IBOutlet weak var lblChordName: UILabel!
     
     override func viewDidLoad() {
@@ -28,16 +27,39 @@ class GuitarViewController: UIViewController {
     @IBAction func chordButtonPressed(_ sender: UIButton) {
         //Cuando se presiona el boton del acorde cambiamos la propiedad text de lblChordName por el título del boton presionado
         lblChordName.text = sender.titleLabel?.text
+        setChordImage(sender.tag)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func setChordImage(_ tag: Int) {
+        
+        switch tag {
+        case 1:
+            imgGuitarChord.image = UIImage(named: "img_g_do")
+        case 2:
+            imgGuitarChord.image = UIImage(named: "img_g_re")
+        case 3:
+            imgGuitarChord.image = UIImage(named: "img_g_rem")
+        case 4:
+            imgGuitarChord.image = UIImage(named: "img_g_mi")
+        case 5:
+            imgGuitarChord.image = UIImage(named: "img_g_mim")
+        case 6:
+            imgGuitarChord.image = UIImage(named: "img_g_fa")
+        case 7:
+            imgGuitarChord.image = UIImage(named: "img_g_fasm")
+        case 8:
+            imgGuitarChord.image = UIImage(named: "img_g_sol")
+        case 9:
+            imgGuitarChord.image = UIImage(named: "img_g_la")
+        case 10:
+            imgGuitarChord.image = UIImage(named: "img_g_lam")
+        case 11:
+            imgGuitarChord.image = UIImage(named: "img_g_si")
+        case 12:
+            imgGuitarChord.image = UIImage(named: "img_g_sim")
+        default:
+            imgGuitarChord.image = UIImage(named: "img_mastil")
+        }
     }
-    */
 
 }
